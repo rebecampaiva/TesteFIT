5626f625df251225b486808ebb9b2f3cbc0471eb
Rebeca Paiva <rebeca.mpaiva@gmail.com>
2021-08-16 08:57:13 -0300

    Adicionar arquivos de projeto.

---
+++ Palindromo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Primo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0; int c = 0; int divisor = 0;
+            Console.Write("Informe um número:");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int a = 1; a < n; a++ )
+            {
+                if (n % a == 0)
+                    divisor++;
+                
+            }
+            if (divisor == 2)
+                Console.WriteLine("O número " + n + " não é primo. Número de iterações necessárias: " + divisor);
+
+            else
+                Console.WriteLine("O número " + n + " é primo. Número de iterações necessárias: 2 - ele mesmo e 1.");
+        }
+    }
+}
---
+++ Primo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = string.Empty;
+            Console.Write("Escreva uma palavra/frase:");
+
+            texto  = Console.ReadLine();
+            Console.WriteLine("É palíndromo? {0} = {1} ", texto, EPalindromo(texto));
+
+            }
+
+        static bool EPalindromo(string texto)
+        {
+            char[] carac = texto.ToCharArray();
+
+            for (int i = 0, j = carac.Length - 1; i < j; i++, j--)
+            {
+                if (carac[i] != carac[j])
+                    return false;
+
+            }
+            return true;
+        }
+    }
+}
